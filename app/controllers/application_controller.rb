66375816e655c6b6a20406b3335c029b30fe2646
CBreakr <github email address>
2020-03-28 18:21:03 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+
+    def current_user
+        @user
+    end
 
     def check_login
         if !session[:user_id].present? then
